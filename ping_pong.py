31e6f158b0b211a9610f9a5f8d675ffa971dbf6d
Who-Is-Rem <alvin.lee724422@gmail.com>
2024-03-08 18:57:35 -0500

    added movement to the paddle, needs to have a higher update rate ad it seems jittery

--- ping_pong.py
+++ ping_pong.py
@@ -6,32 +6,52 @@ class PingPong(QWidget):
     def __init__(self): 
         super(PingPong, self).__init__()
         self.ui()
+        self.pong_stats()
+
+        self.show()
         
     def ui(self):
         self.score = 0
         self.setWindowState(Qt.WindowState.WindowFullScreen)
         self.setWindowTitle("Ping Pong")
-
-        rec = self.rect()
-        self.maxw = rec.width()
-        self.maxh = rec.height()
+        self.setStyleSheet("background-color:black;")
+        
+    def pong_stats(self):
+        self.px = (self.width()/2)-100
+        self.py = self.height()
+        self.spd = 50;
 
     def keyPressEvent(self, event):
         k = event.key()
         if k == Qt.Key.Key_Left:
-            print("left")
+            self.px -= self.spd
+            self.update()
         if k == Qt.Key.Key_Right:
-            print("right")
+            self.px += self.spd
+            self.update()
         
     def keyReleaseEvent(self, event):
-        print("released")
+        k = event.key()
+        if k == Qt.Key.Key_Left:
+            self.update()
+        elif k == Qt.Key.Key_Right:
+            self.update()
 
     def paintEvent(self, event):
-        pass 
+        print("paint")
+        painter = QPainter()
+        painter.begin(self)
+        self.paint_paddle(painter)
+        painter.end()
 
     def paint_paddle(self, painter): 
-        pass 
+        color = QColor(0x808080)
+        painter.fillRect(self.px, self.py, 200, 10, color)
 
+    def resizeEvent(self, event):
+        self.px = (self.width()/2)-100
+        self.py = self.height()-200
+        self.update()
 
 if __name__ == "__main__": 
     import sys 
