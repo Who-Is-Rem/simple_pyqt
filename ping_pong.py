89fd73de553ad0adc612965281f611dccb912bfb
Who-Is-Rem <alvin.lee724422@gmail.com>
2024-03-08 18:11:30 -0500

    Modified event handlers to see personally how they work instead of copy and pasting

--- ping_pong.py
+++ ping_pong.py
@@ -2,87 +2,41 @@ from PySide6.QtWidgets import *
 from PySide6.QtCore import *
 from PySide6.QtGui import *
 
-class PingPong(QMainWindow):
-    def __init__(self):
+class PingPong(QWidget):
+    def __init__(self): 
         super(PingPong, self).__init__()
- 
-        self.board = Board(self)
-
-        self.setCentralWidget(self.board)
-        self.board.start()
-
+        self.ui()
+        
+    def ui(self):
+        self.score = 0
         self.setWindowState(Qt.WindowState.WindowFullScreen)
         self.setWindowTitle("Ping Pong")
 
-class Board(QFrame):
-    def __init__(self, parent): 
-        super(Board, self).__init__(parent)
+        rec = self.rect()
+        self.maxw = rec.width()
+        self.maxh = rec.height()
 
-        self.timer = QBasicTimer()
+    def keyPressEvent(self, event):
+        k = event.key()
+        if k == Qt.Key.Key_Left:
+            print("left")
+        if k == Qt.Key.Key_Right:
+            print("right")
         
-        # Have directions 0,1: left or right  
-        self.direction = 0
-        self.dx = (self.contentsRect().width()/2)-50
+    def keyReleaseEvent(self, event):
+        print("released")
 
-        # Board parameters 
-        self.rec = self.contentsRect()
-        self.maxx = self.rec.width()
-        self.maxy = self.rec.height()
+    def paintEvent(self, event):
+        pass 
 
-    def start(self):
-        self.timer.start(50, self)
-
-    def paint(self, event): 
-        # make painter 
-        painter = QPainter(self)
-
-        self.draw_paddle(painter, (self.maxx/2)-50, self.rec.bottom()+100)
-
-
-    def draw_paddle(self, painter, x, y): 
-        #color 
-        color = QColor(0x808080)
-        painter.fillRect(x, y, 100, 5, color)
-        
-    def key_press(self, event): 
-        key = event.key()
-
-        if key == Qt.Key.Key_Left:
-            self.direction = 0 
-        elif key == Qt.Key.Key_Right:
-            self.direction = 1
-        else:
-            self.direction = 2
-
-    def move_paddle(self): 
-        if self.direction == 0:
-            if self.dx <= 0:
-                self.dx = 0
-            else: 
-                self.dx -= 10 
-        elif self.direction == 1: 
-            if self.dx >= self.maxx:
-                self.dx = 0
-            else:
-                self.dx += 10
-        elif self.direction == 2:
-            pass
-
-    def timerEvent(self, event): 
-        if event.timerId() == self.timer.timerId():
-            self.move_paddle()
-            self.update()
-            
-
-        
-
+    def paint_paddle(self, painter): 
+        pass 
 
 
 if __name__ == "__main__": 
     import sys 
     app = QApplication([])
     pong = PingPong()
-    pong.show()
     sys.exit(app.exec())
 
 
