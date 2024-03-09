2035df753df7ea0e3dbcbac8d1e4081fe1cd752c
Who-Is-Rem <alvin.lee724422@gmail.com>
2024-03-09 18:54:33 -0500

    modified to make multiple players with acceleration, deceleration is broken

--- ping_pong.py
+++ ping_pong.py
@@ -1,71 +1,150 @@
-from PySide6.QtWidgets import *
-from PySide6.QtCore import *
 from PySide6.QtGui import *
+from PySide6.QtCore import *
+from PySide6.QtWidgets import *
 
-class PingPong(QWidget):
-    def __init__(self): 
-        super(PingPong, self).__init__()
-        self.timer = QBasicTimer()
-        self.ui()
-        self.pong_stats()
+"""
+Class for the main window for the Ping Pong Game
+"""
+class PingPong(QMainWindow):
+    def __init__(self, num_players=0):
+        super().__init__()
+        assert num_players >=0 and num_players < 3
+        self.num_players = num_players
 
-        self.start()
-        self.show()
-        
-    def ui(self):
-        self.score = 0
-        self.setWindowState(Qt.WindowState.WindowFullScreen)
+        # initialize window data
         self.setWindowTitle("Ping Pong")
+        self.setWindowState(Qt.WindowState.WindowFullScreen)
         self.setStyleSheet("background-color:black;")
-        
-    def pong_stats(self):
-        self.px = (self.width()/2)-100
-        self.dx = 0
-        self.refresh = 50
 
-    def start(self):
-        self.timer.start(self.refresh, self)
+        # timer for modifying update() rate
+        self.timer = QBasicTimer()
+        self.timer.start(50, self)
 
-    def keyPressEvent(self, event):
-        k = event.key()
-        if k == Qt.Key.Key_Left:
-            self.dx = -50
-        if k == Qt.Key.Key_Right:
-            self.dx = 50
-        
-    def keyReleaseEvent(self, event):
-        k = event.key()
-        if k == Qt.Key.Key_Left or k == Qt.Key.Key_Right:
-            self.dx = 0
+        # set of keys to know what keys have been pressed
+        self.keys = set()
 
-    def timerEvent(self, event):
-        if event.timerId() == self.timer.timerId():
-            self.update()
+        # initialize the players
+        self.initPlayer()
 
+        self.show()
+
+    """
+    initialize the players' data such as position, key presses, and the players themself
+    """
+    def initPlayer(self):
+        self.players = []
+        player_ypos = [self.height()-150, 150]
+        player_xpos = (self.width()/2)-100
+        player_keys = [(Qt.Key.Key_Left, Qt.Key.Key_Right), (Qt.Key.Key_A, Qt.Key.Key_D)]
+        for i in range(self.num_players):
+            self.players.append(Player(player_xpos, player_ypos[i], player_keys[i][0], player_keys[i][1]))
+
+    """
+    Event to paint the players on the main window whenever update() gets triggered
+    """
     def paintEvent(self, event):
         painter = QPainter()
         painter.begin(self)
-        self.paint_paddle(painter)
+        for p in self.players:
+            painter.fillRect(p.px, p.py, p.width, p.height, QColor(0x808080))
         painter.end()
 
-    def paint_paddle(self, painter): 
-        color = QColor(0x808080)
-        self.px += self.dx
-        painter.fillRect(self.px, self.height()-200, 200, 10, color)
+    """
+    Event to increase the speed of update triggering with a timer
+    """
+    def timerEvent(self, event):
+        if event.timerId() == self.timer.timerId():
+            # update player speeds
+            for p in self.players:
+                p.px += p.speed
+            self.update()
 
+
+    """
+    Detect key presses and modify the players' speeds accordingly
+    """
+    def keyPressEvent(self, event):
+        self.keys.add(event.key())
+        for p in self.players:
+            if not (p.left and p.right in self.keys):
+                if p.left in self.keys:
+                    p.accelerate(-1)
+                if p.right in self.keys:
+                    p.accelerate(1)
+
+    """
+    Detect key releases and decrease the players' speeds if neither of the respective keys for
+    the players are pressed
+    """
+    def keyReleaseEvent(self, event):
+        self.keys.remove(event.key())
+        # decelerate for existing players if no key is pressed for respective player
+        for p in self.players:
+            if p.left and p.right not in self.keys:
+                p.accelerate(0)
+
+    """
+    Change the player position when the window resets in size
+    """
     def resizeEvent(self, event):
-        self.px = (self.width()/2)-100
-        self.update()
-
-if __name__ == "__main__": 
-    import sys 
-    app = QApplication([])
-    pong = PingPong()
-    sys.exit(app.exec())
+        player_xpos = (self.width()/2)-100
+        for p in self.players:
+            p.px = player_xpos
+        if self.players[0] != 0:
+            self.players[0].py = self.height()-150
 
 
+"""
+Class for players i.e. the paddles in the game
+"""
+class Player():
+    def __init__(self, x, y, left:Qt.Key, right:Qt.Key):
+        # data for positions
+        self.direction = 2
+        self.px = x
+        self.py = y
 
+        # data for size
+        self.width = 250
+        self.height = 10
 
+        # data for speed
+        self.speed = 0
+        self.max_spd = 60
+        self.acceleration = 5
 
+        # movement keys
+        self.left = int(left)
+        self.right = int(right)
 
+    """
+    Modifies speed and position according to the direction of acceleration
+    described by num
 
+    num:
+    0 - decelerate
+    1 - accelerate in the +x
+    -1 - accelerate in the -x
+    """
+    def accelerate(self, num=0):
+        # accelerate
+        if abs(self.speed) < self.max_spd:
+            if num == 1:
+                self.speed += self.acceleration
+            elif num == -1:
+                self.speed -= self.acceleration
+        # decelerate
+        if num == 0:
+            if self.speed != 0:
+                tsp = abs(self.speed) - self.acceleration
+                if self.speed < 0:
+                    self.speed = -tsp
+                else:
+                    self.speed = tsp
+            elif abs(self.speed) < 5:
+                self.speed = 0
+
+if __name__ == "__main__":
+    app = QApplication()
+    pong = PingPong(1)
+    app.exec()
